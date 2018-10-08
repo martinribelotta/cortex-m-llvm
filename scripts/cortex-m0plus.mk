3a61fa847de9c88e2bf4af3ab051385c73ccd9fe
Martin Ribelotta <martinribelotta@yahoo.com.ar>
2018-10-08 13:25:10 -0300

    Add cortex-m0/m0plus support in scripts
    Refer to issue #3 for better reference

---
+++ scripts/cortex-m0plus.mk
@@ -0,0 +1,6 @@
+ARCH_FLAGS := --target=thumbv6m-unknown-none-eabi
+ARCH_FLAGS += -mthumb
+ARCH_FLAGS += -march=armv6m
+ARCH_FLAGS += -mcpu=cortex-m0plus
+ARCH_FLAGS += -mfloat-abi=soft
+ARCH_FLAGS += -mfpu=none
